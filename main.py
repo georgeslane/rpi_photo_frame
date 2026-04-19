9e26fdfc64e6d2e2507fcd9cc1b1a32413798170
georgeslane <117913042+georgeslane@users.noreply.github.com>
2026-04-20 00:55:34 +0100

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from photo_display import Display
-PATH ='42_octillion_quarks_in_love/'
-
-PhotoFrame = Display()
-images = PhotoFrame.get_photos(path=PATH)
-PhotoFrame.display_slideshow(images=images, path=PATH)
